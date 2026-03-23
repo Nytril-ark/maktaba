85b3d027a3b45f02d18e38c05ad19ead49afc235
MohmmedAtef <mohammedateftawfic@gmail.com>
2026-03-23 18:06:38 +0200

    add login and signup js

---
+++ src/login.js
@@ -0,0 +1,21 @@
+
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const emailInput = document.getElementById("email").value;
+    const passwordInput = document.getElementById("password").value;
+
+    const rawData = localStorage.getItem("AllUsers");
+    const allUsers = JSON.parse(rawData) || [];
+
+    const foundUser = allUsers.find(user => user.Email === emailInput);
+
+    if (foundUser) {
+        if (foundUser.Password === passwordInput) {
+            window.location.replace("index.html");
+        } else {
+            alert("Password is Wrong");
+        }
+    } else {
+        alert("Account not found");
+    }
+});
---
+++ src/signup.js
@@ -0,0 +1,38 @@
+document.querySelector( 'form' ).addEventListener( 'submit', function ( e ) {
+    e.preventDefault();
+    const passwordInput = document.getElementById("password").value;
+    const confirmPasswordInput = document.getElementById( "confirmPassword" ).value;
+    if ( passwordInput !== confirmPasswordInput ) {
+        alert( "Password and Confirm Password is different" );
+        return;
+}
+    let user = {
+        FirstName: document.getElementById( "fname" ).value,
+        LastName: document.getElementById( "lname" ).value,
+        Birthday: document.getElementById( "birthday" ).value,
+        UserName: document.getElementById( "username" ).value,
+        Email: document.getElementById( "email" ).value,
+        Password: passwordInput,
+    }
+
+    
+    
+    var allUsers = JSON.parse( localStorage.getItem( "AllUsers" ) ) || [];
+    
+    if ( allUsers.find( u => u.Email === user.Email ) ) {
+        alert("This email is already registered!");
+        return;
+    }
+    
+    if ( allUsers.find( u => u.UserName === user.UserName ) ) {
+        alert("This Username is already Taken");
+        return;
+    }
+    
+    allUsers.push( user );
+    
+    localStorage.setItem( "AllUsers", JSON.stringify( allUsers ) );
+    alert("Account created successfully!");
+    
+    window.location.replace( "login.html" );
+});
