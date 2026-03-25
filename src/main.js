6f44e84f9a719b4f87a7eeafbe2b9306e555afa6
MohmmedAtef <mohammedateftawfic@gmail.com>
2026-03-25 14:02:40 +0200

    Add signout section on nav bar

--- src/main.js
+++ src/main.js
@@ -6,8 +6,21 @@ if ( localStorage.getItem( "Role" ) !== "Admin" ) {
     document.getElementById( "admin_dashboard" ).style.display = "none";
 }
 
+if ( localStorage.getItem( "Role" ) !== "Guest" ) {
+    document.getElementById( "sign_up" ).style.display = "none";
+    document.getElementById( "login" ).style.display = "none";
+} else {
+    document.getElementById( "sign_out" ).style.display = "none";
+}
+
+document.getElementById( "sign_out" ).addEventListener( "click", function ( e ) {
+    localStorage.setItem( "Role", "Guest" );
+    localStorage.removeItem( "LogedUser" );
+    window.location.replace( "login.html" );
+} );
 
 var allUsers = JSON.parse( localStorage.getItem( "AllUsers" ) ) || [];
+
 if ( allUsers.length === 0 ) {
     
     let adminUser = {
