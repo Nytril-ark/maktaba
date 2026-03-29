10651717d0a2e928ddacd86906cb41f7f0b385e1
NourEldeenGamal <nour2552005@gmail.com>
2026-03-29 14:35:26 +0200

    modified search.js to work with browse page changes

--- src/js/search.js
+++ src/js/search.js
@@ -1,5 +1,9 @@
 import { getBooks } from "./main.js";
 
+let books = [];
+let currentPage = 1;
+const BooksPerPage = 20;
+
 document.addEventListener("DOMContentLoaded",() => {
     const mainSearch = document.querySelector(".searchInput");
     const btnSearch = document.querySelector(".searchIcon");
@@ -11,7 +15,7 @@ document.addEventListener("DOMContentLoaded",() => {
         }
     }
 
-    btnSearch?.addEventListener("click", redirect());
+    btnSearch?.addEventListener("click", redirect);
 
     mainSearch?.addEventListener("keypress", (e) => {
         if(e.key ==="Enter"){
@@ -20,69 +24,184 @@ document.addEventListener("DOMContentLoaded",() => {
     })
 })
 
-let books = [];
-
-function rendertable(displayBook){
-    const table = document.getElementById("booksTable");
-
-    let html = `
-        <tr>
-        <th>Title</th>
-        <th>Author</th>
-        <th>Category</th>
-        <th>Status</th>
-        <th>Details</th>
-        </tr>`
-        displayBook.forEach(book => {
-            html += `
-        <tr>
-        <td align="center">${book.title}</td>
-        <td align="center">${book.authors? book.authors.join(" , "): "unknown"}</td>
-        <td align="center">${book.category}</td>
-        <td align="center">${book.status}</td>
-        <td align="center"><a href="book.html?id=${book.id}">View details</a></td>
-        </tr>`;
+function categoryListeners(){
+    const categoryButtons = document.querySelectorAll(".categories-buttons");
+    categoryButtons.forEach(button1 => {
+        button1.addEventListener("click", () => {
+            categoryButtons.forEach(button2 => button2.classList.remove("active"))
+            button1.classList.add("active");
+            currentPage = 1;
+                filter();
         })
-        table.innerHTML = html;
+    })
+}
+
+function Pagination(Items){
+    const pagination = document.getElementById("pageNumber");
+    pagination.innerHTML = "";
+    const tPages = Math.ceil(Items/BooksPerPage);
+    
+    if(tPages <= 1){
+        return;
+    }
+
+    const range = 4;
+    let pages = [1,2,3,4];
+    pages.push(tPages);
+    
+    if(currentPage < tPages - range){
+        for(let i = currentPage; i <= currentPage + range;i++){
+            if(i >= 1 && i < tPages){
+                pages.push(i);
+            }
+        }
+    }
+    else{
+        for(let i = currentPage; i >= tPages - range;i--){
+            if(i >= 1 && i <= tPages){
+                pages.push(i);
+            }
+        }                
+    }
+    pages = [...new Set(pages)].sort((a,b) => a - b);
+
+    const backButton = document.createElement("button");
+    backButton.textContent = "<";
+    backButton.className = "back";
+    backButton.addEventListener("click", () => {
+        if(currentPage > 1){
+        currentPage = currentPage - 1;
+        filter();
+        window.scrollTo(0, 0);
+        }    
+    })
+    pagination.appendChild(backButton);
+
+    pages.forEach((page,idx) => {
+        if(idx > 0 ){
+            const seperation = document.createElement("span");
+            if(page - pages[idx - 1] > 1){
+            seperation.textContent = ".....";
+            seperation.className = "dotseparate";
+            }
+            else{
+            seperation.textContent = " ";
+            seperation.className = "gapseparate";
+            }    
+            pagination.appendChild(seperation);
+        }
+
+
+
+    const pageButton = document.createElement("button");
+    pageButton.textContent = page;
+    pageButton.className = `page-button ${page === currentPage? "active":""}`;
+
+    pageButton.addEventListener("click", () => {
+        currentPage = page;
+        filter();
+        window.scrollTo(0, 0);
+    })
+    pagination.appendChild(pageButton); 
+    })
+
+    const nextButton = document.createElement("button");
+    nextButton.textContent = ">";
+    nextButton.className = "back";
+    nextButton.addEventListener("click", () => {
+        if(currentPage < tPages){
+        currentPage = currentPage + 1;
+        filter();
+        window.scrollTo(0, 0);
+        }    
+    })
+    pagination.appendChild(nextButton);
+
+}
+
+function renderGrid(displayBook){
+    const Grid = document.getElementById("booksGrid");
+    Grid.innerHTML="";
+    displayBook.forEach(book => {
+    const card = `
+        <div class="book-card" onclick="window.location.href='book.html?id=${book.id}'">
+            <div class="card-img-container">
+                <img src="${book.image?book.image:"../images/index-img2.jpg"}" alt="${book.title}" >
+            </div>
+            <div class="card-content">
+                <h3 class="card-title">${book.title}</h3>
+ 
+                <div class="card-footer">
+                    <span class="card-status status-${book.status.toLowerCase().replace(/\s/g, '-')}">${book.status}</span>
+                    <a href="book.html?id=${book.id}" class="button-details">View Details</a>
+                </div>
+            </div>
+        </div>`;
+        Grid.insertAdjacentHTML("beforeend",card);
+    })
+}
+
+function categorySideBar(books){
+    const categorylist = document.querySelector(".category-list");
+    const categories = 
+    new Set(books.map(book => book.category).filter(Boolean));
+
+    categorylist.innerHTML = 
+    ` <li><button class="categories-buttons active">All books</button></li>`;
+
+    categories.forEach(Name => {
+        const List = document.createElement("li");
+        List.innerHTML = `<button class="categories-buttons">${Name}</button>` 
+        categorylist.appendChild(List);
+    })
+    categoryListeners();
 }
 
 function filter(){
-    const tQuery = (document.getElementById("Search")?.value
-    || document.querySelector(".searchInput")?.value).toLowerCase();
-    const cQuery = document.getElementById("Search-Category")?.value.toLowerCase();
+    const sQuery = (document.querySelector(".searchInput")?.value 
+    || "").toLowerCase();
+    
+    const active = document.querySelector(".categories-buttons.active")
+    const category = active? active.textContent.trim().toLowerCase():"all books";
 
     const filtered = books.filter( book => {
-        const matchText = book.title.toLowerCase().includes(tQuery)
-        || book.authors.some(a => a.toLowerCase().includes(tQuery));
-
-        const matchCategory = book.category.toLowerCase().includes(cQuery)
-        || cQuery === "";
+        const matchText = book.title.toLowerCase().includes(sQuery)
+        || book.authors.some(a => a.toLowerCase().includes(sQuery));
+        const bookCategory = book.category? 
+        book.category.trim().toLowerCase():"";
+        const Allbooks = category ==="all books";
+        const matchCategory = (Allbooks)
+        || (bookCategory === category);
 
         return matchText&&matchCategory;
     });
-    rendertable(filtered);
+
+    const startIdx = (currentPage - 1) * BooksPerPage;
+    const endIdx = startIdx + BooksPerPage;
+    const bookPages = filtered.slice(startIdx, endIdx);
+
+    Pagination(filtered.length);
+    renderGrid(bookPages);
 }
 
 async function pre_search(){
     books = await getBooks();
-    
+
+    categorySideBar(books);
+
     const parameters = new URLSearchParams(window.location.search);
     const URLquery = parameters.get('q');
-    if(URLquery){
-        const browseSearch = document.getElementById("Search");
-        if(browseSearch){
-            browseSearch.value = URLquery;
-        }
-    }
-    filter();
-
     const mainSearch = document.querySelector(".searchInput");
-    const browseSearch = document.getElementById("Search");
-    const categorySearch = document.getElementById("Search-Category");
+    if(URLquery && mainSearch){
+        mainSearch.value = URLquery;
+    }
 
-    [mainSearch, browseSearch, categorySearch].forEach(
-        input => {input?.addEventListener("input", filter);}
-    );
+    mainSearch?.addEventListener("input",() => {
+        currentPage = 1;
+        filter();
+    })
+
+    filter();
 }
 
 pre_search();
