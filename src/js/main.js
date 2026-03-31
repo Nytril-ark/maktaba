5d18b03fe85b6b97ccdb0363681fc8491597b915
MohmmedAtef <mohammedateftawfic@gmail.com>
2026-04-01 01:26:22 +0200

    Solve problems

--- src/js/main.js
+++ src/js/main.js
@@ -1,98 +1,97 @@
-document.addEventListener( "DOMContentLoaded", () => { 
-const role = localStorage.getItem( "Role" ) || "Guest";
+document.addEventListener( "DOMContentLoaded", () => {
+    const role = localStorage.getItem( "Role" ) || "Guest";
 
-const loginButton = document.getElementById( "login" );
-const logoutButton = document.getElementById( "logoutButton" );
+    const loginButton = document.getElementById( "login" );
+    const logoutButton = document.getElementById( "logoutButton" );
 
-const profileSection = document.getElementById( "profileSection" );
-const profileIcon = document.getElementById( "profileIcon" );
-const dropDownMenu = document.getElementById( "dropDownMenu" );
+    const profileSection = document.getElementById( "profileSection" );
+    const profileIcon = document.getElementById( "profileIcon" );
+    const dropDownMenu = document.getElementById( "dropDownMenu" );
 
-const mobileNav = document.getElementById( "Mobile-view-menu" );
-const wrapperNav = document.getElementById( "wrapper-nav" );
-const closeBtn = document.getElementById("close-button");
-const navBorrowed = document.getElementById( "navborrowed" );
+    const mobileNav = document.getElementById( "Mobile-view-menu" );
+    const wrapperNav = document.getElementById( "wrapper-nav" );
+    const closeBtn = document.getElementById( "close-button" );
+    const navBorrowed = document.getElementById( "navborrowed" );
 
-const username = document.getElementById( "username" );
-const email = document.getElementById( "email" );
+    const username = document.getElementById( "username" );
+    const email = document.getElementById( "email" );
 
 
-let logedUser = JSON.parse( localStorage.getItem( "LogedUser" ) ) || null;
+    let logedUser = JSON.parse( localStorage.getItem( "LogedUser" ) ) || null;
 
-if ( !role ) {
-    localStorage.setItem( "Role", "Guest" );
-}
-
-if ( role === "Admin" ) {
-    if ( !window.location.href.includes("admin_dashboard.html") && !window.location.href.includes("book_inventory.html") && !window.location.href.includes("add_book.html") && !window.location.href.includes("edit_book.html") ) {
-        window.location.replace("admin_dashboard.html");
-    }
-}
-
-if ( role !== "Guest" ) {
-if ( loginButton ) {
-    loginButton.style.display = "none";
-}
-
-if ( profileSection ) {
-    profileSection.style.display = "block";
-}
-}
-
-if ( logoutButton ) {
-    logoutButton.addEventListener( "click", function ( e ) {
-        e.preventDefault();
+    if ( !role ) {
         localStorage.setItem( "Role", "Guest" );
-        localStorage.removeItem( "LogedUser" );
-        window.location.replace( "index.html" );
-    });
-}
-
-if ( profileIcon ) {
-    profileIcon.addEventListener( "click", function ( e ) {
-        e.preventDefault();
-        dropDownMenu.classList.toggle( "show" );
-    });
-}
-
-if ( username ) {
-    if (logedUser) {    
-        username.textContent = logeUser.UserName;
     }
-}
 
-if ( email ) {
-    if (logedUser) {    
-        email.textContent = logeUser.Email;
-    }
-}
-
-if ( mobileNav && wrapperNav) {
-    mobileNav.addEventListener( "click", function ( e ) {
-        e.preventDefault();
-        wrapperNav.classList.add( "active" );
-    } );
-}
-if ( closeBtn && wrapperNav) {
-    closeBtn.addEventListener( "click", function ( e ) {
-        wrapperNav.classList.remove( "active" );
-    })
-}
-
-if ( navBorrowed ) {
-    navBorrowed.addEventListener( "click", function ( e ) {
-        e.preventDefault(); 
-        if ( role === "Guest" ) {
-            alert( "You have to login first" );
-            window.location.replace( "login.html" );
-        } else {
-            window.location.replace( "BorrowedBooks.html" );
+    if ( role === "Admin" ) {
+        if ( !window.location.href.includes( "admin_dashboard.html" ) && !window.location.href.includes( "book_inventory.html" ) && !window.location.href.includes( "add_book.html" ) && !window.location.href.includes( "edit_book.html" ) ) {
+            window.location.replace( "admin_dashboard.html" );
         }
-    });
-}
+    }
+
+    if ( role !== "Guest" ) {
+        if ( loginButton ) {
+            loginButton.style.display = "none";
+        }
+
+        if ( profileSection ) {
+            profileSection.style.display = "block";
+        }
+    }
+
+    if ( logoutButton ) {
+        logoutButton.addEventListener( "click", function ( e ) {
+            e.preventDefault();
+            localStorage.setItem( "Role", "Guest" );
+            localStorage.removeItem( "LogedUser" );
+            window.location.replace( "index.html" );
+        } );
+    }
+
+    if ( profileIcon ) {
+        profileIcon.addEventListener( "click", function ( e ) {
+            e.preventDefault();
+            dropDownMenu.classList.toggle( "show" );
+        } );
+    }
+
+    if ( username ) {
+        if ( logedUser ) {
+            username.textContent = logedUser.UserName;
+        }
+    }
+
+    if ( email ) {
+        if ( logedUser ) {
+            email.textContent = logedUser.Email;
+        }
+    }
+
+    if ( mobileNav && wrapperNav ) {
+        mobileNav.addEventListener( "click", function ( e ) {
+            e.preventDefault();
+            wrapperNav.classList.add( "active" );
+        } );
+    }
+    if ( closeBtn && wrapperNav ) {
+        closeBtn.addEventListener( "click", function ( e ) {
+            wrapperNav.classList.remove( "active" );
+        } )
+    }
+
+    if ( navBorrowed ) {
+        navBorrowed.addEventListener( "click", function ( e ) {
+            e.preventDefault();
+            if ( role === "Guest" ) {
+                alert( "You have to login first" );
+                window.location.replace( "login.html" );
+            } else {
+                window.location.replace( "BorrowedBooks.html" );
+            }
+        } );
+    }
 
 } );
-
 
 var allUsers = JSON.parse( localStorage.getItem( "AllUsers" ) ) || [];
 
