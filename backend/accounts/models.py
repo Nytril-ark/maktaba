802fc8eb74c2a339dde8479c3c97bf8c7e2d5b91
Adham El-Sharkawy <adham.elsharkawy0666@gmail.com>
2026-05-06 10:54:52 +0300

    phone number and birth date

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     email=models.EmailField(unique=True)
+    birth_date=models.DateField(null=True,blank=True)
+    phone_number=models.CharField(max_length=15,null=True,blank=True)
     USERNAME_FIELD='email'
     REQUIRED_FIELDS = ['username']
     def __str__(self):
